72e075f61aa177dd5f4fb87b45467545bc1a2beb
duncte123 <contact@duncte123.me>
2021-01-04 20:37:26 +0100

    And that one

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { MarathonService } from '../../services/marathon.service';
 import { Router } from '@angular/router';
