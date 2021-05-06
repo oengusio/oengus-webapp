c12ea89c29022f57d35ec29e27decb7acd70d6ca
duncte123 <contact@duncte123.me>
2021-05-06 11:41:33 +0200

    Fix not being able to submit

--- src/services/submission.service.ts
+++ src/services/submission.service.ts
@@ -2,115 +2,80 @@ import { Injectable } from '@angular/core';
 import { Submission } from '../model/submission';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
-import { NwbAlertConfig, NwbAlertService } from '@wizishop/ng-wizi-bulma';
+import { NwbAlertService } from '@wizishop/ng-wizi-bulma';
 import { TranslateService } from '@ngx-translate/core';
+import {BaseService} from './BaseService';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SubmissionService {
+export class SubmissionService extends BaseService {
 
   constructor(private http: HttpClient,
-              private toastr: NwbAlertService,
+              toastr: NwbAlertService,
               private translateService: TranslateService) {
+    super(toastr, 'marathons');
   }
 
   mine(marathonId: string): Observable<Submission> {
-    return this.http.get<Submission>(environment.api + '/marathon/' + marathonId + '/submissions/me');
+    return this.http.get<Submission>(this.url(`${marathonId}/submissions/me`));
   }
 
   availabilities(marathonId: string): Observable<any> {
-    return this.http.get<any>(environment.api + '/marathon/' + marathonId + '/submissions/availabilities');
+    return this.http.get<any>(this.url(`${marathonId}/submissions/availabilities`));
   }
 
   submissions(marathonId: string): Observable<Submission[]> {
-    return this.http.get<Submission[]>(environment.api + '/marathon/' + marathonId + '/submissions');
+    return this.http.get<Submission[]>(this.url(`${marathonId}/submissions`));
   }
 
   answers(marathonId: string): Observable<Submission[]> {
-    return this.http.get<Submission[]>(environment.api + '/marathon/' + marathonId + '/submissions/answers');
+    return this.http.get<Submission[]>(this.url(`${marathonId}/submissions/answers`));
   }
 
   availabilitiesForUser(marathonId: string, userId: number): Observable<any> {
-    return this.http.get<any>(environment.api + '/marathon/' + marathonId + '/submissions/availabilities/' + userId);
+    return this.http.get<any>(this.url(`${marathonId}/submissions/availabilities/${userId}`));
   }
 
   create(marathonId: string, submission: Submission) {
     return this.http
-      .post(environment.api + '/marathon/' + marathonId + '/submissions', submission, {observe: 'response'})
+      .post(this.url(`${marathonId}/submissions`), submission, {observe: 'response'})
       .subscribe((response: any) => {
         this.translateService.get('alert.submission.save.success').subscribe((res: string) => {
-          const alertConfig: NwbAlertConfig = {
-            message: res,
-            duration: 3000,
-            position: 'is-right',
-            color: 'is-success'
-          };
-          this.toastr.open(alertConfig);
+          this.toast(res);
         });
       }, error => {
         this.translateService.get('alert.submission.save.error').subscribe((res: string) => {
-          const alertConfig: NwbAlertConfig = {
-            message: res,
-            duration: 3000,
-            position: 'is-right',
-            color: 'is-warning'
-          };
-          this.toastr.open(alertConfig);
+          this.toast(res, 3000, 'warning');
         });
       });
   }
 
   update(marathonId: string, submission: Submission) {
     return this.http
-      .put(environment.api + '/marathon/' + marathonId + '/submissions', submission, {observe: 'response'})
+      .put(this.url(`${marathonId}/submissions`), submission, {observe: 'response'})
       .subscribe((response: any) => {
         this.translateService.get('alert.submission.save.success').subscribe((res: string) => {
-          const alertConfig: NwbAlertConfig = {
-            message: res,
-            duration: 3000,
-            position: 'is-right',
-            color: 'is-success'
-          };
-          this.toastr.open(alertConfig);
+          this.toast(res);
         });
       }, error => {
         this.translateService.get('alert.submission.save.error').subscribe((res: string) => {
-          const alertConfig: NwbAlertConfig = {
-            message: res,
-            duration: 3000,
-            position: 'is-right',
-            color: 'is-warning'
-          };
-          this.toastr.open(alertConfig);
+          this.toast(res, 3000, 'warning');
         });
       });
   }
 
   delete(marathonId: string, submissionId: number, callback?) {
-    return this.http.delete(environment.api + '/marathon/' + marathonId + '/submissions/' + submissionId).subscribe(response => {
+    return this.http.delete(this.url(`${marathonId}/submissions/${submissionId}`)).subscribe(response => {
       this.translateService.get('alert.submission.deletion.success').subscribe((res: string) => {
-        const alertConfig: NwbAlertConfig = {
-          message: res,
-          duration: 3000,
-          position: 'is-right',
-          color: 'is-success'
-        };
-        this.toastr.open(alertConfig);
+        this.toast(res);
         if (callback) {
           callback();
         }
       });
     }, error => {
       this.translateService.get('alert.submission.deletion.error').subscribe((res: string) => {
-        const alertConfig: NwbAlertConfig = {
-          message: res,
-          duration: 3000,
-          position: 'is-right',
-          color: 'is-warning'
-        };
-        this.toastr.open(alertConfig);
+        this.toast(res, 3000, 'warning');
       });
     });
   }
