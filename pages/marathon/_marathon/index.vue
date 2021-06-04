97d7a2b1f1328b7b45d45cf0c8348f38275887dd
BobChao87 <bobchao87@gmail.com>
2021-06-04 01:29:15 -0700

    Attaches stub URL param extraction to dynamic base pages
    This demonstration is helpful to confirm static routes are safe
    
    Related to Issue #13

--- pages/marathon/_marathon/index.vue
+++ pages/marathon/_marathon/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    Oengus marathon main page stub
+    <p>Oengus marathon main page stub</p>
+    <p>This is for marathon "{{marathon}}."</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  asyncData({ params }) {
+    return {
+      marathon: params.marathon,
+    };
+  },
+});
+</script>
--- pages/user/_user.vue
+++ pages/user/_user.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    Oengus user profile page stub
+    <p>Oengus user profile page stub</p>
+    <p>This is for user "{{user}}."</p>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  asyncData({ params }) {
+    return {
+      user: params.user,
+    };
+  },
+});
+</script>
