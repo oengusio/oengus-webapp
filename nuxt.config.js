8e6dc768d3bbe710a04f5ecf00f3f9ed595a045a
BobChao87 <bobchao87@gmail.com>
2021-06-08 01:08:21 -0700

    Pull in Bulma, BulmaSwatch, and set Solar theme.
    Related to Issue #11

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~assets/global',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
