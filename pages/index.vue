2396ed543603e2253a0598a0b61fd22a6437c42b
BobChao87 <bobchao87@gmail.com>
2021-07-04 02:51:35 -0700

    Fixes an issue with linting imports from nuxt-i18n
    Related to Issue #12

--- pages/index.vue
+++ pages/index.vue
@@ -7,26 +7,46 @@
         {{ locale.code }}
       </option>
     </select>
+    <div>
+      <input v-model="username">
+      <button @click="getUserExists(username)">
+        Get User
+      </button>
+      <p>{{ exists }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapActions } from 'vuex';
+/* eslint-disable-next-line import/named */ /* ESLint has trouble with this import... */
 import { LocaleObject } from 'nuxt-i18n';
 
 export default Vue.extend({
+  data() {
+    return {
+      username: '',
+    };
+  },
   computed: {
-    locales(): Array<LocaleObject|string> {
-      return this.$i18n.locales;
+    locales(): Array<LocaleObject> {
+      return this.$i18n.locales as Array<LocaleObject>;
     },
     currentLocale(): string {
       return this.$i18n.locale;
+    },
+    exists(): string {
+      return this.$store.state.api.user.exists[this.username] ? 'True' : 'False';
     },
   },
   methods: {
     changeLocale({ target }: { target: HTMLSelectElement }): void {
       this.$i18n.setLocale(target.value);
     },
+    ...mapActions({
+      getUserExists: 'api/user/exists',
+    }),
   },
 });
 </script>
