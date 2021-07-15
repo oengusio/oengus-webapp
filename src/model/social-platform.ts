cc28632a293db57eb9a3c90425976129d589a2b7
duncte123 <contact@duncte123.me>
2021-07-15 19:27:59 +0200

    Remove phone from profile

--- src/model/social-platform.ts
+++ src/model/social-platform.ts
@@ -7,19 +7,5 @@ export enum SocialPlatform {
   SNAPCHAT = 'https://www.snapchat.com/add/',
   DISCORD = '',
   EMAIL = 'mailto:',
-  PHONE = 'tel:',
   NICO = 'https://com.nicovideo.jp/community/',
-}
-
-export enum SocialPlatformName {
-  SPEEDRUNCOM = 'speedrun.com',
-  TWITTER = 'Twitter',
-  TWITCH = 'Twitch',
-  FACEBOOK = 'Facebook',
-  INSTAGRAM = 'Instagram',
-  SNAPCHAT = 'Snapchat',
-  DISCORD = 'Discord',
-  EMAIL = 'Email',
-  PHONE = 'Phone Number',
-  NICO = 'ニコニココミュニティ',
 }
