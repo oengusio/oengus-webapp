7fecf4cd92c502351933d6ed6beb86a9f22f6d5d
BobChao87 <bobchao87@gmail.com>
2021-07-30 02:23:48 -0700

    Cookies element can turn on or leave off tracking
    Cookies hides by default, including if the user has DNT
    
    Related to Issuse #22, #26

--- components/global/Cookies.vue
+++ components/global/Cookies.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="level box has-dark-background">
+  <!-- Use v-show over v-if since ssr cannot detect this property properly -->
+  <div class="level box has-dark-background" v-show="showConsentPrompt">
     <div class="level-left">
       {{ $t('navbar.privacyConsent') }}
     </div>
     <div class="buttons are-small level-right">
-      <button class="button is-primary">
+      <button class="button is-primary" @click="setCookies(true)">
         {{ $t('action.accept') }}
       </button>
-      <button class="button is-warning">
+      <button class="button is-warning" @click="setCookies(false)">
         {{ $t('action.decline') }}
       </button>
       <nuxt-link :to="localePath('/about')" class="button is-info">
@@ -16,3 +17,34 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      showConsentPrompt: false,
+    };
+  },
+  beforeMount(): void {
+    let showConsentPrompt: boolean = true;
+    // doNotTrack is deprecated, let's not break if it gets removed
+    showConsentPrompt &&= navigator?.doNotTrack !== '1';
+    showConsentPrompt &&= localStorage.getItem('consent') === null;
+    this.showConsentPrompt = showConsentPrompt;
+  },
+  methods: {
+    setCookies(consent: boolean): void {
+      // Add any other tracking schemes here. Hopefully I can find a simple way to iterate all of them.
+      // We invert for these, because this are DISABLE flags. Consent: yes; disable: no
+      (window as any)['ga-disable-G-26CN947SSZ'] = !consent;
+      (window as any)['ga-disable-UA-153189507-4'] = !consent;
+
+      localStorage.setItem('consent', consent.toString());
+
+      this.showConsentPrompt = false;
+    },
+  },
+});
+</script>
