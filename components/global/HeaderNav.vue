afe31b0abb5d29a633c883b82953a980e2822009
BobChao87 <bobchao87@gmail.com>
2021-07-31 02:46:39 -0700

    Add Oengus brand information to header
    Related to Issue #22

--- components/global/HeaderNav.vue
+++ components/global/HeaderNav.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    Header Nav
-  </div>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <nuxt-link :to="localePath('/')" class="navbar-item icon-text">
+        <img src="/oengus.svg" class="icon" alt="Oengus logo">
+        <span>
+          Oengus
+        </span>
+      </nuxt-link>
+    </div>
+  </nav>
 </template>
