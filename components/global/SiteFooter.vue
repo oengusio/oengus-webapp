0092e6e68d6d51f57431bc3961fe8698893153d4
BobChao87 <bobchao87@gmail.com>
2021-08-01 23:59:23 -0700

    Configures site footer
    Includes timezone detection, which might be more more centralized later
    
    Related to Issue #22

--- components/global/SiteFooter.vue
+++ components/global/SiteFooter.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    Footer
-  </div>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        {{ $t('footer.text', { timezone: $moment.tz.guess() }) }}
+      </p>
+      <p v-html="$t('footer.donate', { donationLink: 'https://www.patreon.com/oengusio' })" />
+      <p>
+        &copy; {{ thisYear }}
+        <a href="https://esamarathon.com/">
+          European Speedrunner Assembly AB
+        </a>
+      </p>
+    </div>
+  </footer>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      thisYear: new Date().getFullYear(),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+    padding-bottom: 4rem;
+  }
+</style>
