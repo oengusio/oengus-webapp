16534185ed471517969b711ad77982bc00567616
BobChao87 <bobchao87@gmail.com>
2021-08-06 01:54:13 -0700

    Group the nav icons together for mobile
    Related to Issue #22

--- components/header/Nav.vue
+++ components/header/Nav.vue
@@ -16,24 +16,26 @@
 
     <div class="navbar-menu" :class="isActiveClass">
       <div class="navbar-start">
-        <a href="https://discord.gg/ZZFS8YT" class="navbar-item" target="_blank">
-          <FontAwesomeIcon :icon="[ 'fab', 'discord' ]" class="icon" />
-        </a>
-        <a href="https://twitter.com/OengusIO" class="navbar-item" target="_blank">
-          <FontAwesomeIcon :icon="[ 'fab', 'twitter' ]" class="icon" />
-        </a>
-        <a href="https://github.com/esamarathon/oengusio" class="navbar-item" target="_blank">
-          <FontAwesomeIcon :icon="[ 'fab', 'github' ]" class="icon" />
-        </a>
-        <a href="https://github.com/esamarathon/oengusio/issues" class="navbar-item" target="_blank">
-          <FontAwesomeIcon :icon="[ 'fas', 'bug' ]" class="icon" />
-        </a>
-        <a href="https://hosted.weblate.org/projects/oengusio/site/" class="navbar-item" target="_blank">
-          <FontAwesomeIcon :icon="[ 'fas', 'language' ]" class="icon" />
-        </a>
-        <a href="https://www.patreon.com/oengusio" class="navbar-item" target="_blank">
-          <FontAwesomeIcon :icon="[ 'fab', 'patreon' ]" class="icon" />
-        </a>
+        <div class="navbar-item field is-grouped">
+          <a href="https://discord.gg/ZZFS8YT" class="navbar-item" target="_blank">
+            <FontAwesomeIcon :icon="[ 'fab', 'discord' ]" class="icon" />
+          </a>
+          <a href="https://twitter.com/OengusIO" class="navbar-item" target="_blank">
+            <FontAwesomeIcon :icon="[ 'fab', 'twitter' ]" class="icon" />
+          </a>
+          <a href="https://github.com/esamarathon/oengusio" class="navbar-item" target="_blank">
+            <FontAwesomeIcon :icon="[ 'fab', 'github' ]" class="icon" />
+          </a>
+          <a href="https://github.com/esamarathon/oengusio/issues" class="navbar-item" target="_blank">
+            <FontAwesomeIcon :icon="[ 'fas', 'bug' ]" class="icon" />
+          </a>
+          <a href="https://hosted.weblate.org/projects/oengusio/site/" class="navbar-item" target="_blank">
+            <FontAwesomeIcon :icon="[ 'fas', 'language' ]" class="icon" />
+          </a>
+          <a href="https://www.patreon.com/oengusio" class="navbar-item" target="_blank">
+            <FontAwesomeIcon :icon="[ 'fab', 'patreon' ]" class="icon" />
+          </a>
+        </div>
       </div>
       <div class="navbar-end">
         <NuxtLink :to="localePath('/about')" class="navbar-item">
