1686eb0692fcf30cb9c005741e62442bb9a5845d
BobChao87 <bobchao87@gmail.com>
2021-08-08 01:57:40 -0700

    Kaspersky sponsor box for homepage
    Related to Issue #15

---
+++ components/homepage/Kaspersky.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <a href="https://www.kaspersky.com/?ref=oengus.io" target="_blank" class="image">
+      <img src="/kaspersky_logo.png" alt="Kaspersky Logo">
+    </a>
+    <p v-html="$t('homepage.sponsors.kaspersky.box', { readMoreUrl: localePath('/news/kaspersky') })" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .image {
+    display: flex;
+    justify-content: center;
+
+    > img {
+      max-height: 60px;
+      width: auto;
+    }
+  }
+</style>
