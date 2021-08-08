29a9256feaedbe83efa6d8e288ad62399eebb242
BobChao87 <bobchao87@gmail.com>
2021-08-08 01:57:57 -0700

    General Sponsors box for homepage
    Related to Issue #15

---
+++ components/homepage/Sponsors.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="box">
+    <h5 class="title">
+      {{ $t('homepage.sponsors.title') }}
+    </h5>
+    <p class="icon-text">
+      <img src="~/assets/ESA-Logo.svg" alt="European Speedrunner Assembly Logo" class="icon">
+      <span v-html="$t('homepage.sponsors.esa')" />
+    </p>
+  </div>
+</template>
