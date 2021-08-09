204d81135dca1b0b68f7a1dd45dd0c1cae993145
BobChao87 <bobchao87@gmail.com>
2021-08-09 02:33:43 -0700

    Updates LanguagePicker to use the new Dropdown
    Related to Issue #22

--- components/widget/LanguagePicker.vue
+++ components/widget/LanguagePicker.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="navbar-item has-dropdown" :class="isActiveClass">
-    <a class="navbar-link" @click="toggleActive">
+  <ElementDropdown type="navbar" is-right>
+    <template #trigger>
       {{ currentLocale.nativeName }}
-    </a>
-    <div class="navbar-dropdown is-right">
+    </template>
+    <template #options>
       <a v-for="locale in locales" :key="locale.code" class="navbar-item" @click="changeLocale(locale)">
         {{ locale.nativeName }}
       </a>
-    </div>
-  </div>
+    </template>
+  </ElementDropdown>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 /* eslint-disable-next-line import/named */ /* ESLint has trouble with this import... */
 import { LocaleObject } from 'nuxt-i18n';
-import { IsActive } from '~/types/components/is-active';
 
 export default Vue.extend({
   data() {
     return {
       locales: this.$i18n.locales as Array<LocaleObject>,
-      isActive: false,
     };
   },
   computed: {
@@ -29,19 +27,10 @@ export default Vue.extend({
       // While .find() can normally return undefined, it shouldn't be possible here, so assert found
       return this.locales.find(locale => locale.code === this.$i18n.locale)!;
     },
-    isActiveClass(): IsActive {
-      return {
-        'is-active': this.isActive,
-      };
-    },
   },
   methods: {
     changeLocale(locale: LocaleObject): void {
       this.$i18n.setLocale(locale.code);
-      this.isActive = false;
-    },
-    toggleActive(): void {
-      this.isActive = !this.isActive;
     },
   },
 });
