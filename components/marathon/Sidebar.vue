d0c1292f2f83aef51d69dd5328a092aaa90c25eb
BobChao87 <bobchao87@gmail.com>
2021-08-11 01:31:31 -0700

    Adds MarathonSidebar component
    Related to Issue #16

---
+++ components/marathon/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="menu box">
+    <h4 class="title is-4">
+      {{ marathon }} Name
+    </h4>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      marathon: this.$route.params.marathon,
+    };
+  },
+});
+</script>
--- pages/marathon/_marathon.vue
+++ pages/marathon/_marathon.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
-    <div>
-      Marathon pretty formatting helper, away!
-    </div>
-    <NuxtChild />
+  <div class="marathon-container">
+    <MarathonSidebar class="marathon-sidebar" />
+    <NuxtChild class="marathon-view" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .marathon-container {
+    display: flex;
+    gap: var(--spacing);
+  }
+
+  .marathon-sidebar {
+    width: 350px;
+  }
+
+  @media (max-width: 1023px) {
+    .marathon-container {
+      flex-direction: column;
+    }
+
+    .marathon-sidebar {
+      width: 100%;
+    }
+  }
+</style>
