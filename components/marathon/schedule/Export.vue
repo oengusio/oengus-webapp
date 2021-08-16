0651e979c69ad6ff66270d474e2219b9d75b92da
BobChao87 <bobchao87@gmail.com>
2021-08-15 23:34:02 -0700

    Marathon Schedule Export component
    Takes advantage of API return types to simplify code
    
    Related to Issue #16

---
+++ components/marathon/schedule/Export.vue
@@ -0,0 +1,30 @@
+<template>
+  <ElementDropdown is-right>
+    <template #trigger>
+      {{ $t('marathon.schedule.export.title') }}
+    </template>
+    <template #options>
+      <a v-for="format in formats" :key="format" :href="getExportUrl(format)" target="_blank" class="dropdown-item">
+        {{ $t(`marathon.schedule.export.${format}`) }}
+      </a>
+    </template>
+  </ElementDropdown>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      formats: [ 'ics', 'csv', 'json' ],
+    };
+  },
+  methods: {
+    getExportUrl(format: string): string {
+      // Update to point to ENV API location once we split them up
+      return `https://oengus.io/api/marathons/${this.$route.params.marathon}/schedule/export?format=${format}&zoneId=${(this.$moment as any).tz.guess()}&locale=${this.$i18n.locale}`;
+    },
+  },
+});
+</script>
--- pages/marathon/_marathon/schedule/index.vue
+++ pages/marathon/_marathon/schedule/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    Oengus marathon schedule view page stub
+    <div class="level">
+      <div class="level-left">
+        <h3 class="title is-3 level-item">
+          {{ $t('marathon.schedule.title') }}
+        </h3>
+      </div>
+      <div class="level-right">
+        <MarathonScheduleExport class="level-item" />
+      </div>
+    </div>
   </div>
 </template>
