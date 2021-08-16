de5f753d3afd2066771416c4c2f1feb99efe8187
BobChao87 <bobchao87@gmail.com>
2021-08-16 00:23:18 -0700

    Current/Next runs container and spacing
    Related to Issue #16

--- components/marathon/schedule/Current.vue
+++ components/marathon/schedule/Current.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    Current/upcoming/single run info block
+    <div class="message" :class="messageClass">
+      <div class="message-header">
+        {{ $t(messageHeaderTitle, messageHeaderArgs) }}
+      </div>
+      <div class="message-body">
+        Run information here
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    isNext: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    messageClass(): string {
+      return this.isNext ? '' : 'is-primary';
+    },
+    messageHeaderTitle(): string {
+      return this.isNext ? 'marathon.schedule.nextRun' : 'marathon.schedule.currentRun';
+    },
+    messageHeaderArgs(): { duration?: string } {
+      return this.isNext ? { duration: 'SOON' } : { };
+    },
+  },
+});
+</script>
--- pages/marathon/_marathon/schedule/index.vue
+++ pages/marathon/_marathon/schedule/index.vue
@@ -10,5 +10,29 @@
         <MarathonScheduleExport class="level-item" />
       </div>
     </div>
+
+    <div class="current-runs-container">
+      <MarathonScheduleCurrent />
+      <MarathonScheduleCurrent is-next />
+    </div>
+
+    <p>
+      {{ $t('marathon.schedule.timezone') }}
+    </p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .current-runs-container {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: stretch;
+    gap: var(--spacing);
+
+    > * {
+      min-width: min(400px, 100%);
+      flex-grow: 1;
+    }
+  }
+</style>
