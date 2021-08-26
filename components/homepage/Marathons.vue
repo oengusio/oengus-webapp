1aa0e5c6a6b606b708c1379d5ec7f095a5eef12d
BobChao87 <bobchao87@gmail.com>
2021-08-26 15:28:19 -0700

    Forcibly align homepage marathons listings
    Switches "global" to "desktop" icon for online events.
    Causes location and language tags to be closer together.
    Start on darker line color, to make it be more distinct pre-table update
    
    Related to Issue #15

--- components/homepage/Marathons.vue
+++ components/homepage/Marathons.vue
@@ -9,27 +9,33 @@
       </span>
     </h3>
 
-    <div v-if="homepageMarathons.live.length">
-      <h3 class="title is-3">
-        {{ $t('homepage.marathons.live') }}
-      </h3>
-      <HomepageMarathonsList :marathons="homepageMarathons.live" time-translation-key="homepage.ends" />
-      <br>
-    </div>
-
-    <div v-if="homepageMarathons.next.length">
-      <h4 class="title is-4">
-        {{ $t('homepage.marathons.upcoming') }}
-      </h4>
-      <HomepageMarathonsList :marathons="homepageMarathons.next" time-translation-key="homepage.starts" />
-      <br>
-    </div>
-
-    <div v-if="homepageMarathons.open.length">
-      <h4 class="title is-4">
-        {{ $t('homepage.marathons.open') }}
-      </h4>
-      <HomepageMarathonsList :marathons="homepageMarathons.open" time-translation-key="homepage.submissions_close" />
+    <div class="marathons-list-container">
+      <template v-for="marathonsList in marathonsLists">
+        <template v-if="homepageMarathons[marathonsList.key].length">
+          <h4 :key="marathonsList.key + 'title'" class="title" :class="marathonsList.headerClass">
+            {{ $t(marathonsList.label) }}
+          </h4>
+          <div :key="marathonsList.key + 'prespacer'" class="spacer" />
+          <template v-for="(marathon, index) in homepageMarathons[marathonsList.key]">
+            <span :key="marathonsList.key + 'name' + index" class="notification" :class="getRowParity(index)">
+              <NuxtLink :to="localePath(`/marathon/${marathon.id}`)">
+                {{ marathon.name }}
+              </NuxtLink>
+            </span>
+            <span :key="marathonsList.key + 'location' + index" class="notification location" :class="getRowParity(index)">
+              <span v-if="marathon.onsite" class="icon flag-icon" :class="`flag-icon-${marathon.country.toLowerCase()}`" />
+              <FontAwesomeIcon v-else :icon="[ 'fas', 'desktop' ]" class="icon" />
+            </span>
+            <span :key="marathonsList.key + 'language' + index" class="notification" :class="getRowParity(index)">
+              {{ marathon.language.toLocaleUpperCase() }}
+            </span>
+            <span :key="marathonsList.key + 'time' + index" class="notification time" :class="getRowParity(index)">
+              {{ $t(marathonsList.timeTranslationKey, { duration: 'DURATION' }) }}
+            </span>
+          </template>
+          <div :key="marathonsList.key + 'postspacer'" class="spacer" />
+        </template>
+      </template>
     </div>
   </div>
 </template>
@@ -42,7 +48,34 @@ export default Vue.extend({
   data() {
     return {
       homepageMarathons,
+      marathonsLists: [
+        {
+          key: 'live',
+          label: 'homepage.marathons.live',
+          timeTranslationKey: 'homepage.ends',
+          headerClass: 'is-3',
+        },
+        {
+          key: 'next',
+          label: 'homepage.marathons.upcoming',
+          timeTranslationKey: 'homepage.starts',
+          headerClass: 'is-4',
+        },
+        {
+          key: 'open',
+          label: 'homepage.marathons.open',
+          timeTranslationKey: 'homepage.submissions_close',
+          headerClass: 'is-4',
+        },
+      ],
     };
+  },
+  methods: {
+    getRowParity(index: number): { 'is-dark': boolean } {
+      return {
+        'is-dark': index % 2 === 0,
+      };
+    },
   },
 });
 </script>
@@ -50,5 +83,41 @@ export default Vue.extend({
 <style lang="scss" scoped>
   .homepage-marathons-container {
     width: 100%;
+  }
+
+  .marathons-list-container {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(4, auto);
+    grid-template-columns: auto min-content min-content auto;
+    grid-auto-rows: auto;
+
+    > * {
+      // A lot of the notification styling is undesirable in this context
+      padding: calc(var(--spacing) / 2);
+      margin-block-end: 0;
+      border-radius: 0;
+
+      &.title {
+        // Span from start to finish
+        grid-column: 1 / -1;
+        padding-inline-start: 0;
+      }
+
+      &.location {
+        display: flex;
+        justify-content: center;
+      }
+
+      &.time {
+        text-align: end;
+      }
+
+      &.spacer {
+        // Span from start to finish
+        grid-column: 1 / -1;
+        height: var(--spacing);
+      }
+    }
   }
 </style>
