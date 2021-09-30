be84e3abb0eca6050b353a59fbbfd6678095a72c
BobChao87 <bobchao87@gmail.com>
2021-09-29 21:04:48 -0700

    Anonymize IP for Google GTag
    Related to Issue #26

--- configs/google-gtag.config.js
+++ configs/google-gtag.config.js
@@ -4,10 +4,16 @@ export function googleGtagConfig() {
   return {
     // "New Style" GA4 Data Stream
     id: 'G-26CN947SSZ',
+    config: {
+      anonymize_ip: true,
+    },
     additionalAccounts: [
       {
         // "Old Style" Web Property
         id: 'UA-153189507-4',
+        config: {
+          anonymize_ip: true,
+        },
       },
     ],
   };
