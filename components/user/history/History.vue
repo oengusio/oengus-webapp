81ab83589c039a4b4441bb82b247c25bc4f41e0e
BobChao87 <bobchao87@gmail.com>
2021-10-29 02:01:23 -0700

    User history container with tabs!
    Related to Issue #39

--- components/user/history/History.vue
+++ components/user/history/History.vue
@@ -1,26 +1,29 @@
 <template>
-  <div>
+  <div class="history-container">
     <div class="tabs is-boxed">
       <ul>
-        <li>
-          <a>
-            Submission History
-          </a>
+        <li :class="isActiveClass(submissionTab)">
+          <ElementLink :to="{ query: { ...$route.query, [tabQuery]: submissionTab } }">
+            {{ $t('user.profile.submissions') }}
+          </ElementLink>
         </li>
-        <li>
-          <a>
-            Moderated Marathons
-          </a>
+        <li :class="isActiveClass(moderationTab)">
+          <ElementLink :to="{ query: { ...$route.query, [tabQuery]: moderationTab } }">
+            {{ $t('user.profile.moderator') }}
+          </ElementLink>
         </li>
       </ul>
     </div>
-    <UserHistorySubmission :user-id="userId" />
-    <UserHistoryModeration :user-id="userId" />
+    <div class="history-view-container">
+      <UserHistorySubmission v-show="isActive(submissionTab)" :user-id="userId" />
+      <UserHistoryModeration v-show="isActive(moderationTab)" :user-id="userId" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { IsActive } from '~/types/components/is-active';
 
 export default Vue.extend({
   props: {
@@ -29,5 +32,58 @@ export default Vue.extend({
       default: '',
     },
   },
+
+  data() {
+    return {
+      tabQuery: 'user-history',
+      submissionTab: 'submission',
+      moderationTab: 'moderation',
+    };
+  },
+
+  computed: {
+    activeTab(): string|Array<string|null> {
+      return this.$route.query[this.tabQuery] ?? this.submissionTab;
+    },
+  },
+
+  methods: {
+    isActive(tabName: string): boolean {
+      return this.activeTab === tabName;
+    },
+    isActiveClass(tabName: string): IsActive {
+      return {
+        'is-active': this.activeTab === tabName,
+      };
+    },
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+@use '~bulmaswatch/solar/variables' as solar;
+
+.history-container {
+  padding-block: var(--spacing);
+}
+
+.tabs {
+  margin: 0;
+
+  li a {
+    background-color: solar.$grey-dark;
+  }
+
+  li.is-active a {
+    color: solar.$yellow-invert;
+    background-color: solar.$yellow;
+  }
+}
+
+.history-view-container {
+  border-color: solar.$grey-dark;
+  border-style: solid;
+  border-width: 0 2px 2px;
+  padding: var(--spacing);
+}
+</style>
