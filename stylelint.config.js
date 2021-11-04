26e60420d60a7b2b9c5de6ffe7a6c11301bb934c
BobChao87 <bobchao87@gmail.com>
2021-11-04 03:26:22 -0700

    Add new Table mixins for shrinking
    This will allow consistent, reusable behavior throughout the project
    
    Related to Issue #43

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,8 +5,22 @@ module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: [ 'blockless-after-same-name-blockless', 'first-nested' ],
+        ignore: [ 'after-comment' ],
+        ignoreAtRules: [ 'if', 'else' ],
+      },
+    ],
     // SCSS uses @rules. Some are unknown.
     'at-rule-no-unknown': null,
+    'block-closing-brace-newline-after': [
+      'always',
+      {
+        ignoreAtRules: [ 'if', 'else' ],
+      },
+    ],
     'declaration-block-single-line-max-declarations': 0,
     // Allow ::v-deep ( >>> and /deep/ both fail more fundamentally )
     'selector-pseudo-element-no-unknown': [
