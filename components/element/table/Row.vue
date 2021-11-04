92d0413c936cf34bc045bc59c4bc76924a17f708
BobChao87 <bobchao87@gmail.com>
2021-11-04 03:27:47 -0700

    New Table Row element
    Allows for safe embedding of Table Cells in an encapsulated way
    Allows for certain CSS properties to be passed along to the Cells
    Allows for how row interactions
    
    Related to Issue #43

---
+++ components/element/table/Row.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="element-table-row">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '~assets/table';
+
+.element-table-row {
+  @include table.cell-varients($selector-after: '::v-deep > .element-table-cell');
+
+  // XXX display: contents should be a hack and an implementation detail and is subject to change
+  // This is a substitute for properly using subgrid (CSS Grid Level 2)
+  display: contents;
+}
+</style>
