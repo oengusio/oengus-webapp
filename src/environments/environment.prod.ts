db72db1fb52e76b018b8de910b7b3599ae161f03
duncte123 <contact@duncte123.me>
2021-12-30 13:40:55 +0100

    Make sure the redirects work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,11 +8,15 @@ export const environment = {
   api: 'https://oengus.io/api',
   twitterClientId: 'Z0Zta2JiTjNLQU5iNHlBbGcyLUI6MTpjaQ',
   twitchClientId: 'f4ry4d9k0dt89ha8ks7cb845trvbod',
-  loginRedirect: 'https://oengus.io/login/',
-  syncRedirect: 'https://oengus.io/user/settings/sync/',
   discordClientId: '559625844197163008',
   paypalClientId: 'AfkLlDPvmM0v1914bih8VN0x0LZps-jkjdz-A7ydiJ0RVPdaAC730VWtVkTWaXZauBYVs3UbaoRHc6VH',
   patreonClientId: 'qXKJ92UNMBtSTV_N0TN47U6l1o2_VXWmqK3u9_Gv3RXIJuiJ0LTpDyzbl5XZuARH',
+  get loginRedirect() {
+    return environment.baseSite + '/login/';
+  },
+  get syncRedirect() {
+    return environment.baseSite + '/user/settings/sync/';
+  },
   get patronApi() {
     return environment.api + '/patreon';
   },
