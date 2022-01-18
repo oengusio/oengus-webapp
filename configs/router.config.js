992578e4099530acd525ef6a7e5635d05ddd831f
BobChao87 <bobchao87@gmail.com>
2022-01-18 02:24:50 -0800

    Handle the old user profile links

--- configs/router.config.js
+++ configs/router.config.js
@@ -1,7 +1,19 @@
 // Router configuration: https://nuxtjs.org/docs/directory-structure/nuxt-config#router
+import { sortRoutes } from '@nuxt/utils';
 
 export function routerConfig() {
   return {
+    extendRoutes(routes) {
+      routes.push({
+        // Handles old user profiles by pushing them to the new user profiles!
+        path: '/:locale/user/profile/:user',
+        redirect: to => ({
+          path: '/:locale/user/:user',
+          params: to.params,
+        }),
+      });
+      sortRoutes(routes);
+    },
     middleware: 'oengus-v1-redirect',
   };
 }
