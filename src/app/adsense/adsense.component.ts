a2e3f9f24e96cb467f0be25025bc93bd33689336
duncte123 <contact@duncte123.me>
2022-09-03 09:16:34 +0200

    Force google to not take all the add space

--- src/app/adsense/adsense.component.ts
+++ src/app/adsense/adsense.component.ts
@@ -1,10 +1,12 @@
-import { Component, AfterViewInit } from '@angular/core';
+import {Component, AfterViewInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-google-adsense',
   templateUrl: './adsense.component.html',
 })
 export class AdsenseComponent implements AfterViewInit {
+  @Input('maxWidth') maxWidth: string;
+
   constructor() {}
 
   ngAfterViewInit() {
