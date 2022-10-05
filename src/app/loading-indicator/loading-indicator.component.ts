cb9d71f6ca5a92b6bd72740fc6173500ec463e19
Duncan Sterken <contact@duncte123.me>
2022-10-05 15:44:39 +0200

    Fix loading indicator for submissions (#61)

---
+++ src/app/loading-indicator/loading-indicator.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-loading-indicator',
+  templateUrl: './loading-indicator.component.html',
+  styleUrls: ['./loading-indicator.component.scss']
+})
+export class LoadingIndicatorComponent implements OnInit {
+
+  @Input() public loading = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
