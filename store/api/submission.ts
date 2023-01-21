3fef6dc2e00dd18ee2cf64913ad58006469f5e09
duncte123 <contact@duncte123.me>
2023-01-21 08:22:10 +0100

    Add pagination

--- store/api/submission.ts
+++ store/api/submission.ts
@@ -15,11 +15,12 @@ export const mutations: MutationTree<SubmissionState> = {
   },
 };
 
+// TODO: implement pagination
 export const actions: ActionTree<SubmissionState, SubmissionState> = {
-  get: SubmissionOengusAPI.get<Array<Submission>, SubmissionList>({
+  get: SubmissionOengusAPI.get<{ content: Array<Submission> }, SubmissionList>({
     path: 'submissions',
     key: 'submissions',
-    transform: ({ value: submissions }) => ({ submissions }),
+    transform: ({ value }) => ({ submissions: value.content }),
     cacheDuration: 15 * 60_000,
   }),
 };
