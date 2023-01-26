d224882f96ccda3d3d0a2a14088d6bfb39a939fe
duncte123 <contact@duncte123.me>
2023-01-26 08:44:45 +0100

    Update translations

--- src/app/guards/can-activate-marathon-submit-guard.service.ts
+++ src/app/guards/can-activate-marathon-submit-guard.service.ts
@@ -31,6 +31,6 @@ export class CanActivateMarathonSubmitGuard implements CanActivate {
   }
 
   private condition(user: User, marathon: Marathon) {
-    return !!marathon.canEditSubmissions && !!user;
+    return marathon.canEditSubmissions && user !== null;
   }
 }
