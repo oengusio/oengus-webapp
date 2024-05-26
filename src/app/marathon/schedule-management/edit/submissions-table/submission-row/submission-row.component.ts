0b7fc4ee3e6d45fe6accc744716cfa3d16fed8aa
duncte123 <contact@duncte123.me>
2024-05-26 16:00:25 +0200

    Select runner availability in submission table

--- src/app/marathon/schedule-management/edit/submissions-table/submission-row/submission-row.component.ts
+++ src/app/marathon/schedule-management/edit/submissions-table/submission-row/submission-row.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { V2ScheduleLine } from '../../../../../../model/schedule-line';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { LineRunner, V2ScheduleLine } from '../../../../../../model/schedule-line';
+import { faCalendarTimes, faCalendarWeek, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-submission-row',
@@ -9,8 +9,20 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 })
 export class SubmissionRowComponent {
   iconChevronRight = faChevronRight;
+  iconCalendarWeek = faCalendarWeek;
+  iconCalendarTimes = faCalendarTimes;
 
   @Input() i: number;
   @Input() line: V2ScheduleLine;
+  @Input() selectedAvailabilities: Array<string> = [];
   @Output() moveToSchedule = new EventEmitter<number>();
+  @Output() selectAvailability = new EventEmitter<{ username: string, on: boolean }>();
+
+  getRunnerUsername(runner: LineRunner): string {
+    if (runner.profile) {
+      return runner.profile.username;
+    }
+
+    return runner.runnerName;
+  }
 }
--- src/app/marathon/schedule-management/edit/submissions-table/submissions-table.component.ts
+++ src/app/marathon/schedule-management/edit/submissions-table/submissions-table.component.ts
@@ -11,5 +11,7 @@ export class SubmissionsTableComponent {
   public getRowParity = getRowParity;
 
   @Input() todoLines: V2ScheduleLine[] = [];
+  @Input() selectedAvailabilities: Array<string>;
   @Output() moveToSchedule = new EventEmitter<number>();
+  @Output() selectAvailability = new EventEmitter<{ username: string, on: boolean }>();
 }
