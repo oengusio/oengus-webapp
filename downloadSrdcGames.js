e25eae07621333e9cf3617b5df4584c9ec9db3b6
duncte123 <contact@duncte123.me>
2024-06-12 09:33:42 +0200

    Add apple tv and a lot of other consoles

---
+++ downloadSrdcGames.js
@@ -0,0 +1,48 @@
+// Quick script to download consoles from srdc
+
+const https = require('https');
+const fs = require('fs');
+
+const base = 'https://www.speedrun.com/api/v1/platforms?max=200';
+
+async function fetchWithOffset(offset) {
+  return new Promise((resolve, reject) => {
+
+    https.get(`${base}&offset=${offset}`, (res) => {
+      let data = [];
+
+      res.on('data', chunk => {
+        data.push(chunk);
+      });
+
+      res.on('end', () => {
+        console.log('Response ended: ');
+        const jsonData = JSON.parse(Buffer.concat(data).toString());
+
+        resolve(jsonData.data.map(it => it.name));
+
+      });
+    }).on('error', (e) => reject(e));
+
+  })
+}
+
+async function main() {
+  let offset = 0;
+  let lastData = [];
+  let totalData = []
+
+  do {
+    lastData = await fetchWithOffset(offset);
+
+    console.log(lastData);
+
+    totalData.push(...lastData);
+
+    offset += 200;
+  } while (lastData.length > 0);
+
+  fs.writeFileSync('./src/assets/consoles.json', JSON.stringify(totalData, null, 2));
+}
+
+main();
