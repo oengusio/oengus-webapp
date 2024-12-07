4cf753ea65b9cc1c6ab4d302c95e4da8e1f123f1
Duncan Sterken <contact@duncte123.me>
2024-12-07 16:11:43 +0100

    Remove the old schedule UI (#122)
    * First step of removing the old UI
    
    * Remove even more stuff

--- src/services/schedule.service.ts
+++ src/services/schedule.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, tap } from 'rxjs';
 import { NwbAlertService } from '@wizishop/ng-wizi-bulma';
-import { TranslateService } from '@ngx-translate/core';
-import { Schedule, ScheduleCreateRequest, ScheduleInfo, V2Schedule } from '../model/schedule';
+import { ScheduleCreateRequest, ScheduleInfo, V2Schedule } from '../model/schedule';
 import moment from 'moment-timezone';
-import {BaseService} from './BaseService';
+import { BaseService } from './BaseService';
 import { BooleanStatusDto, DataListDto } from '../model/dto/base-dtos';
 import { V2ScheduleLine } from '../model/schedule-line';
 import { map } from 'rxjs/operators';
@@ -17,8 +16,8 @@ export class ScheduleService extends BaseService {
   private scheduleCache: Map<string, Array<ScheduleInfo>> = new Map();
 
   constructor(private http: HttpClient,
-              toastr: NwbAlertService,
-              private translateService: TranslateService) {
+              toastr: NwbAlertService
+  ) {
     super(toastr, 'marathons');
   }
 
@@ -97,52 +96,5 @@ export class ScheduleService extends BaseService {
 
   fetchExport(marathonId: string, scheduleId: number, format: string): Observable<Blob> {
     return this.http.get(this.getExportUrl(marathonId, scheduleId, format), {responseType: 'blob'});
-  }
-
-  /////////////////
-  // V1 stuff below
-
-  /**
-   * @deprecated this only gets the first schedule for a marathon
-   */
-  getAllForMarathon(marathonId: string, customData: boolean = false, adminRoute: boolean = false): Observable<Schedule> {
-    const adminPart = adminRoute ? '/admin' : '';
-    const query = customData ? '?withCustomData=true' : '';
-
-    return this.http.get<Schedule>(this.url(`${marathonId}/schedule${adminPart}${query}`));
-  }
-
-  save(marathonId: string, schedule: Schedule) {
-    const fixedSchedule = Object.assign({}, schedule);
-
-    // @ts-ignore
-    fixedSchedule.lines = fixedSchedule.lines.map((line) => ({
-      ...line,
-      runners: line.runners.map((runner) => {
-        if ('runnerName' in runner) {
-          return {
-            runnerName: runner.runnerName,
-          };
-        }
-
-        return {
-          user: {
-            id: runner.user.id,
-            username: runner.user.username,
-          },
-        };
-      }),
-    }));
-
-    return this.http.put(this.url(`${marathonId}/schedule`), fixedSchedule, {observe: 'response'})
-      .subscribe((response: any) => {
-        this.translateService.get('alert.schedule.save.success').subscribe((res: string) => {
-          this.toast(res);
-      });
-    }, error => {
-      this.translateService.get('alert.schedule.save.error').subscribe((res: string) => {
-        this.toast(res, 3000, 'warning');
-      });
-    });
   }
 }
