132a88bc65a9337cef25726ce341472a4cbbdb41
duncte123 <contact@duncte123.me>
2025-10-23 18:38:27 +0200

    More fixes

--- src/loader/webpack-translate-loader.ts
+++ src/loader/webpack-translate-loader.ts
@@ -1,8 +1,8 @@
-import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslationObject } from '@ngx-translate/core';
 import { from, Observable } from 'rxjs';
 
 export class WebpackTranslateLoader implements TranslateLoader {
-  getTranslation(lang: string): Observable<unknown> {
+  getTranslation(lang: string): Observable<TranslationObject> {
     if (lang === 'en-GB') {
       return from(import(`../assets/i18n/en.json`));
     }
