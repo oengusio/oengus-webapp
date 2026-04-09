7a32ecf3e51f1ff7f4023931b8e09b41c1a00fbd
duncte123 <contact@duncte123.me>
2026-04-09 18:16:45 +0200

    Add script for updating deps

---
+++ bump_angular.fish
@@ -0,0 +1,3 @@
+#!/usr/bin/fish
+
+ng update @angular/common @angular-devkit/build-angular @angular/core @angular/cdk @fortawesome/angular-fontawesome @angular/compiler @angular/cli @angular-eslint/eslint-plugin-template @angular-eslint/template-parser @sentry/angular angular-eslint eslint
